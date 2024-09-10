bbf71376d0e3088b23078b0f4b23cd96b1edada0
Marcos Ferreto Estrada <marcosferretoestrada@gmail.com>
2024-09-10 13:02:51 -0600

    Add a query "url"

--- app/routes.py
+++ app/routes.py
@@ -6,9 +6,20 @@ router = APIRouter()
 
 
 @router.get("/scrape")
-def scrape_endpoint():
+async def scrape_endpoint(url: str):
+    """Handles the scraping of data from a specified URL via an HTTP GET request.
+
+    This asynchronous endpoint receives a URL as a query parameter, invokes the scraping function to retrieve data from that URL, and returns the data in a JSON format. If an error occurs during the scraping process, an error message is returned instead.
+
+    Args:
+        url (str): The URL to scrape data from.
+
+    Returns:
+        dict: A dictionary containing either the scraped data or an error message.
+    """
+
     try:
-        data = scrape_data()  # Ejecutar la función de scraping
+        data = scrape_data(url)  # Ejecutar la función de scraping
         return {"data": data}
     except Exception as e:
         return {"error": str(e)}
--- app/scraping/scraper.py
+++ app/scraping/scraper.py
@@ -2,14 +2,30 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def scrape_data():
+def scrape_data(url: str):
+    """Scrapes data from a specified webpage using Selenium.
+
+    This function navigates to a given URL and extracts data from a table present on the webpage. The extracted data is returned as a list of rows, where each row contains the values of the table cells.
+
+    Args:
+        url (str): The URL of the webpage to scrape data from.
+
+    Returns:
+        list: A list of rows containing the scraped data, where each row is a list of cell values.
+
+    Raises:
+        WebDriverException: If there is an issue with the Selenium WebDriver.
+    """
+
     # Configurar Selenium
     driver = (
         webdriver.Chrome()
     )  # Asegúrate de tener Chromedriver instalado y configurado
-    driver.get(
-        "https://data.wa.gov/Transportation/Electric-Vehicle-Population-Data/f6w7-q2d2"
-    )  # Cambia por la URL del sitio web
+    # driver.get(
+    #    "https://data.wa.gov/Transportation/Electric-Vehicle-Population-Data/f6w7-q2d2"
+    # )  # Cambia por la URL del sitio web
+
+    driver.get(f"{url}")
 
     # Extraer datos usando Selenium (ejemplo)
     elements = driver.find_elements(
